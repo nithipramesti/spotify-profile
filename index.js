c64cf182b926561afe4e1080bea7dec8bc7e377e
nithipramesti <nithipramesti1508@gmail.com>
2022-07-28 09:19:17 +0700

    deploying to heroku

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express = require("express");
 const querystring = require("query-string");
 const app = express();
 const axios = require("axios");
-const port = 8888;
+const path = require("path");
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
+
+//Priority serve any static files
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.get("/", (req, res) => {
   const data = {
@@ -84,7 +89,7 @@ app.get("/callback", (req, res) => {
         });
 
         //redirect to react app
-        res.redirect(`http://localhost:3000/?${queryParams}`);
+        res.redirect(`${FRONTEND_URI}?${queryParams}`);
 
         //pass along tokens in query params
 
@@ -135,6 +140,11 @@ app.get("/refresh_token", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app listening at http://localhost:${PORT}`);
 });
